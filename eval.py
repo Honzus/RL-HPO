98d89ee5a40eef89dfcba9a4a712b22498af8bd5
Honzus <64539119+Honzus@users.noreply.github.com>
2025-11-13 15:16:03 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -16,6 +16,7 @@ import complexmaze
 import simplemaze
 from TabularAgents import TabularRL
 import utils
+import random
 
 SEEDS = [0, 1, 2, 3, 4]
 
@@ -32,8 +33,12 @@ def eval(env_name, config):
 
     for seed in SEEDS:
         np.random.seed(seed)
+        random.seed(seed)
         env = gym.make(env_name) # NChain-v0, KellyCoinflip-v0, Sepsis/ICU-Sepsis-v2
-
+        _, _ = env.reset(seed=seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        
         agent = TabularRL(env, learning_rate, epsilon, discount_factor)
 
         episode_rewards = []
